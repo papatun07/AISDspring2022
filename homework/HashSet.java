c819041460534afba0ef9d93c00605c80cefed0c
arutiun papoyan <90134951+papatun07@users.noreply.github.com>
2022-04-21 11:33:35 +0300

    Create HashSet.java

---
+++ homework/HashSet.java
@@ -0,0 +1,107 @@
+import java.util.ListIterator;
+
+class HashSet<K> extends HashTable<K, K> {
+
+    public HashSet(int size) {
+        super(size);
+    }
+
+    public HashSet() {
+        super();
+    }
+
+    public K get(K key) {
+        for (Pair<K, K> pair : table[getIndex(key)]) {
+            if (pair.getKey().equals(key))
+                return pair.getValue();
+        }
+        return null;
+    }
+
+    public void set(K key) {
+        Pair <K, K> pair = new Pair<>(key, key);
+        ListIterator<Pair<K, K>> i = table[getIndex(key)].listIterator();
+
+        while (i.hasNext()) {
+            if (i.next().equals(pair)) {
+                i.set(pair);
+                return;
+            }
+        }
+        table[getIndex(key)].add(pair);
+    }
+
+    public void del(K key) {
+        ListIterator<Pair<K, K>> i = table[getIndex(key)].listIterator();
+
+        while (i.hasNext()) {
+            if (i.next().getKey().equals(key)) {
+                i.remove();
+                return;
+            }
+        }
+    }
+
+    public boolean contains(K key) {
+        for (Pair<K, K> pair : table[getIndex(key)]) {
+            if (pair.getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public HashSet<K> intersection (HashSet<K> setB) {
+        HashSet<K> newSet = new HashSet<>(size);
+
+        for (int i = 0; i < size; i++) {
+            for (Pair<K, K> pair : table[i]) {
+                if (setB.contains(pair.getKey()))
+                    newSet.set(pair.getKey());
+            }
+        }
+        return newSet;
+    }
+
+    public HashSet<K> difference(HashSet<K> setB) {
+        HashSet<K> newSet = new HashSet<>(size);
+
+        for (int i = 0; i < size; i++) {
+            for (Pair<K,K> pair : table[i]) {
+                if (!setB.contains(pair.getKey()))
+                    newSet.set(pair.getKey());
+            }
+        }
+        return newSet;
+    }
+
+    public HashSet<K> union(HashSet<K> setB) {
+        HashSet<K> newSet = new HashSet<>(size*2);
+
+        for (int i = 0; i < size; i++) {
+            for (Pair<K,K> pair : table[i]) {
+                newSet.set(pair.getKey());
+            }
+        }
+
+        for (int i = 0; i < setB.size; i++) {
+            for (Pair<K,K> pair : setB.table[i]) {
+                newSet.set(pair.getKey());
+            }
+        }
+        return newSet;
+    }
+
+    public String toString() {
+        StringBuilder string = new StringBuilder("[");
+
+        for (int i = 0; i < size; i++) {
+            for (Pair<K,V> pair : table[i]) {
+                string.append(pair.getKey().toString() + ", ");
+            }
+        }
+        if (string.length() > 3)
+            string.delete(string.length() - 2, string.length());
+        return string.append("]").toString();
+    }
+}
