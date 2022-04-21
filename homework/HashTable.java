c8260cd8e64bf3c66c370b62347ed57349406b08
arutiun papoyan <90134951+papatun07@users.noreply.github.com>
2022-04-21 11:31:57 +0300

    Create HashTable.java

---
+++ homework/HashTable.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class HashTable<K,V> {
+    protected int size;
+    protected ArrayList<Pair<K,V>> table[];
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new ArrayList[size];
+
+        for (int i = 0; i < size; i++) {
+            table[i] = new ArrayList<Pair<K,V>>();
+        }
+    }
+    public HashTable() {
+        this(10);
+    }
+    protected int getIndex(K key) {
+        int hash_key = Math.abs(key.hashCode() % size);
+        return hash_key;
+    }
+}
